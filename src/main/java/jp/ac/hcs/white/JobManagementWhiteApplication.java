dec5470a2e571e4af4541b805fcf2f507275a8af
s20183040 <s20183040@HCS19090.stdlan.hcs.local>
2020-07-06 14:26:17 +0900

    初回コミット

---
+++ src/main/java/jp/ac/hcs/white/JobManagementWhiteApplication.java
@@ -0,0 +1,14 @@
+package jp.ac.hcs.white;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobManagementWhiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobManagementWhiteApplication.class, args);
+		System.out.println("aaa");
+			}
+
+}
---
+++ src/test/java/jp/ac/hcs/white/JobManagementWhiteApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.ac.hcs.white;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobManagementWhiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
