3599cc5e0f64d73cdec921fc9aef15921c27da6c
s20183040 <s20183040@HCS19090.stdlan.hcs.local>
2020-07-08 13:59:33 +0900

    aaaa

--- src/main/java/jp/ac/hcs/white/user/UserRepository.java
+++ src/main/java/jp/ac/hcs/white/user/UserRepository.java
@@ -22,14 +22,11 @@ public class UserRepository {
 	/** SQL 1件取得 */
 	private static final String SQL_SELECT_ONE = "SELECT * FROM m_user WHERE user_id = ?";
 
+	/** SQL 1件追加  */
+	private static final String SQL_INSERT_ONE = "INSERT INTO m_user(user_id, encrypted_password, user_name,  user_role, user_class,user_student_no,user_darkmode, user_status) VALUES(?, ?, ?,?,?,?, false, ?)";
+
 	/** SQL 1件更新 管理者 パスワード更新有 */
 	private static final String SQL_UPDATE_ONE_WITH_PASSWORD = "UPDATE m_user SET encrypted_password = ?, user_name = ?, user_role = ? WHERE user_id = ?";
-
-	/** SQL 1件追加  */
-	private static final String SQL_INSERT_ONE = "INSERT INTO m_user(user_id, encrypted_password, user_name, user_darkmode, user_role) VALUES(?, ?, ?, false, ?)";
-
-	/** SQL 1件更新 管理者 パスワード更新無 */
-	private static final String SQL_UPDATE_ONE = "UPDATE m_user SET user_name = ?, user_role = ? WHERE user_id = ?";
 
 	/** SQL 1件削除 */
 	private static final String SQL_DELETE_ONE = "DELETE FROM m_user WHERE user_id = ?";
@@ -63,9 +60,11 @@ public class UserRepository {
 			UserData data = new UserData();
 			data.setUser_id((String) map.get("user_id"));
 			data.setUser_name((String) map.get("user_name"));
+			data.setUser_class((String)map.get("user_class"));
 			data.setUser_darkmode((boolean) map.get("darkmode"));
-			data.setRole((String) map.get("role"));
-
+			data.setRole((String) map.get("user_role"));
+			data.setUser_student_no((int)map.get("user_student_no"));
+			data.setUser_status((String)map.get("user_status"));
 			entity.getUserlist().add(data);
 		}
 		return entity;
@@ -85,38 +84,14 @@ public class UserRepository {
 		return data;
 	}
 
-	public UserEntity selectOne2(String user_id) {
-		List<Map<String, Object>> resultList = jdbc.queryForList(SQL_SELECT_ONE,user_id);
-		UserEntity userEntity = mappingSelectResult(resultList);
-		return userEntity;
-	}
-
-	/**
-	 * (管理用)Userテーブルのデータを1件更新する(パスワード更新有).
-	 * @param data 更新するユーザ情報
-	 * @return 更新データ数
-	 * @throws DataAccessException
-	 */
 	public int updateOneWithPassword(UserData data) throws DataAccessException {
 		int rowNumber = jdbc.update(SQL_UPDATE_ONE_WITH_PASSWORD,
 				passwordEncoder.encode(data.getPassword()),
 				data.getUser_name(),
 				data.getRole(),
+				data.getUser_class(),
+				data.getUser_student_no(),
 				data.getUser_id());
-		return rowNumber;
-	}
-
-	/**
-	 * (管理用)Userテーブルのデータを1件更新する(パスワード更新無).
-	 * @param data 更新するユーザ情報
-	 * @return 更新データ数
-	 * @throws DataAccessException
-	 */
-	public int updateOne(UserData userData) throws DataAccessException {
-		int rowNumber = jdbc.update(SQL_UPDATE_ONE,
-				userData.getUser_name(),
-				userData.getRole(),
-				userData.getUser_id());
 		return rowNumber;
 	}
 
@@ -131,6 +106,8 @@ public class UserRepository {
 						data.getUser_id(),
 						passwordEncoder.encode(data.getPassword()),
 						data.getUser_name(),
+						data.getUser_class(),
+						data.getUser_student_no(),
 						data.getRole());
 
 		return rowNumber;
