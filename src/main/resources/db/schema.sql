1a22e3f197d497bfc674d9fba570067f08501ea3
s20183040 <s20183040@HCS19090.stdlan.hcs.local>
2020-07-10 10:19:35 +0900

    あんぱんまん

--- src/main/resources/db/schema.sql
+++ src/main/resources/db/schema.sql
@@ -30,9 +30,10 @@ CREATE TABLE IF NOT EXISTS examreport (
   report_day DATE,
   recruitment_number INT,
   company_name VARCHAR(50),
-  exam_application_place VARCHAR(50),
+  Application_route VARCHAR(10),
   exam_date_time DATE,
   examination_location VARCHAR(50),
+  co_test VARCHAR(10),
   remarks VARCHAR(600),
   Exam_report_status VARCHAR(10)
 );
