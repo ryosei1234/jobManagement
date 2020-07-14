e18190413c5e374661409cb07d537e063219e35a
s20183010 <s20183010@HCS19073.stdlan.hcs.local>
2020-07-14 11:23:22 +0900

    CSV出力 Controller Sevice WebConfig

--- src/main/java/jp/ac/hcs/white/WebConfig.java
+++ src/main/java/jp/ac/hcs/white/WebConfig.java
@@ -7,6 +7,10 @@ import org.springframework.web.client.RestTemplate;
 @Configuration
 public class WebConfig {
 
+	/** タスク情報のCSVファイル名 */
+	public static final String FILENAME_CSV = "list.csv";
+
+
 	@Bean
 	public RestTemplate restTemplate() {
 
--- src/main/java/jp/ac/hcs/white/examreport/ExamReportService.java
+++ src/main/java/jp/ac/hcs/white/examreport/ExamReportService.java
@@ -1,5 +1,12 @@
 package jp.ac.hcs.white.examreport;
 
+import java.io.IOException;
+import java.nio.file.FileSystem;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+
+import javax.validation.Path;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
 import org.springframework.stereotype.Service;
@@ -49,5 +56,30 @@ public class ExamReportService {
 
 		return examreportEntity;
 	}
+
+	/**
+	 * 情報をCSVファイルとしてサーバに保存する.
+	 * @throws DataAccessException
+	 */
+	public void saveCsv() throws DataAccessException {
+
+		examRepository.saveCsv();
+	}
+
+	/**
+	 * サーバーに保存されているファイルを取得して、byte配列に変換する.
+	 * @param fileName ファイル名
+	 * @return ファイルのbyte配列
+	 * @throws IOException ファイル取得エラー
+	 */
+	public byte[] loadCsv(String fileName) throws IOException {
+
+		FileSystem fs = FileSystems.getDefault();
+		Path p = fs.getPath(fileName);
+		byte[] bytes = Files.readAllBytes(p);
+
+		return bytes;
+	}
+
 
 }
