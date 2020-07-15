77c9121b49c30ec7a1d01848c5e160a1ccb068d6
s20183040 <s20183040@HCS19090.stdlan.hcs.local>
2020-07-15 10:07:12 +0900

    エラーメッセージだどん

--- src/main/java/jp/ac/hcs/white/WebConfig.java
+++ src/main/java/jp/ac/hcs/white/WebConfig.java
@@ -1,7 +1,10 @@
 package jp.ac.hcs.white;
 
+import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
@@ -10,6 +13,26 @@ public class WebConfig {
 	/** タスク情報のCSVファイル名 */
 	public static final String FILENAME_CSV = "list.csv";
 
+	@Bean
+	public MessageSource messageSource() {
+
+		//メッセージプロパティのファイル設定
+		ReloadableResourceBundleMessageSource bean = new ReloadableResourceBundleMessageSource();
+		bean.setBasename("classpath:messages");
+		bean.setDefaultEncoding("UTF-8");
+
+		return bean;
+	}
+
+	@Bean
+	public LocalValidatorFactoryBean localValidatorFactoryBean() {
+
+		// バリデーションのメッセージ設定
+		LocalValidatorFactoryBean localValidatorFactoryBean = new LocalValidatorFactoryBean();
+		localValidatorFactoryBean.setValidationMessageSource(messageSource());
+
+		return localValidatorFactoryBean;
+	}
 
 	@Bean
 	public RestTemplate restTemplate() {
