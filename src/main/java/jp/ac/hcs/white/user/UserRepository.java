c439ed20a9898d5d493b905bbe39974f363b4d64
s20183032 <s20183032@HCS19074.stdlan.hcs.local>
2020-07-27 09:47:06 +0900

    aaaa

--- src/main/java/jp/ac/hcs/white/user/UserRepository.java
+++ src/main/java/jp/ac/hcs/white/user/UserRepository.java
@@ -28,13 +28,13 @@ public class UserRepository {
 
 	/** SQL 1件追加  */
 	private static final String SQL_INSERT_ONE = "INSERT INTO m_user(user_id, encrypted_password, user_name, user_role, user_class, user_student_no, user_darkmode, user_status, created_at, created_user_id, update_at, update_user_id) "
-									+ "VALUES(?, ?, ?, ?, ?, ?, false, 'VALID', ? , ?, ?, ?)";
+									+ "VALUES(?, ?, ?, ?, ?, ?, false, 'VALID', ?, ?, ?, ?)";
 
 	/** SQL 1件更新 パスワード更新有 */
 	private static final String SQL_UPDATE_ONE_WITH_PASSWORD = "UPDATE m_user SET encrypted_password = ?, user_name = ?, user_role = ?, User_class = ?, User_student_no = ? ,update_at = ?,update_user_id = ? WHERE user_id = ?";
 
 	/** SQL 1件更新  パスワード更新無 */
-	private static final String SQL_UPDATE_ONE = "UPDATE m_user SET user_name = ?, user_role = ?, User_class = ?, User_student_no = ? ,update_at = ?,update_user_id = ? WHERE user_id = ?";
+	private static final String SQL_UPDATE_ONE = "UPDATE m_user SET user_name = ?, user_role = ?, User_class = ?, User_student_no = ? ,update_at = ?, update_user_id = ? WHERE user_id = ?";
 
 	/** SQL 1件削除 */
 	private static final String SQL_DELETE_ONE = "DELETE FROM m_user WHERE user_id = ?";
