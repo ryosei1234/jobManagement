6e6970a83e550649f30bada48c408fa98d0337ac
s20183032 <s20183032@HCS19071.stdlan.hcs.local>
2020-10-27 14:42:48 +0900

    Controllerの作成

--- src/main/java/jobhuntingreport/JobHuntingController.java
+++ src/main/java/jobhuntingreport/JobHuntingController.java
@@ -1,6 +1,36 @@
 package jobhuntingreport;
 
+import java.security.Principal;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Controller
 public class JobHuntingController {
 
-	
+
+
+
+
+	/**
+	 * 受験報告一覧を表示
+	 * @param principal ログイン情報
+	 * @param model
+	 * @return 受験報告一覧画面
+	 */
+	@PostMapping("/job/joblist")
+	public String getJobList(Principal principal, Model model) {
+		log.info("[" + principal.getName() + "]就職活動検索" + principal.getName());
+		JobHuntingEntity jobHuntingEntity;
+		jobHuntingEntity = jobHuntingService.selectAll(principal.getName());
+		model.addAttribute("jobHuntingEntity", jobHuntingEntity);
+
+		return "job/joblist";
+	}
+
+
 }
---
+++ src/main/java/jobhuntingreport/JobHuntingEntity.java
@@ -0,0 +1,11 @@
+package jobhuntingreport;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class JobHuntingEntity {
+	/** 就職活動申請情報のリスト */
+	private List<JobHuntingData> joblist = new ArrayList<JobHuntingData>();
+
+
+}
