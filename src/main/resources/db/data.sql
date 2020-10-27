5cf73c0e9629c316788e00cddcbeedb6c507549c
s20183045 <s20183045@HCS19064.stdlan.hcs.local>
2020-10-27 14:35:48 +0900

    就職活動申請・報告マスタの実装、テーブルにサンプルデータを格納しました。

--- src/main/resources/db/data.sql
+++ src/main/resources/db/data.sql
@@ -27,3 +27,15 @@ VALUES('0000000001','yamada@xxx.co.jp','S','エイチ','2020-07-09 12:48:35','00
 
 INSERT INTO  examreport (examreport_id,user_id,department,company_name_top,report_day,recruitment_number,company_name,application_route,exam_date_time,examination_location,contens_test ,remarks,Exam_report_status)
 VALUES('0000000002','suzuki@xxx.co.jp','S','エイチ','2020-07-09 12:48:35','0000000002','株式会社HCS','斡旋サイト','2020-07-17','札幌','1次試験','あんぱんまん','新規作成');
+
+/* 就職活動申請・報告のデータ(休日に実施した場合) */
+INSERT INTO application_and_report(examination_report_id,examination_status_id,action_id,action_place,action_day,action_end_day,company_name,action_status_id,attendance_id,attendance_day,lodging_day_id,information)
+VALUES('0000000010','2','1','北海道情報専門学校','2020-10-31 10:30','2020-10-31 12:30','株式会社HCS','0','0',null,'0');
+
+/* 就職活動申請・報告のデータ(欠席する場合) */
+INSERT INTO application_and_report(examination_report_id,examination_status_id,action_id,action_place,action_day,action_end_day,company_name,action_status_id,attendance_id,attendance_day,lodging_day_id,information)
+VALUES('0000000011','2','1','北海道情報専門学校','2020-11-01 10:30','2020-11-01 12:30','株式会社HCS','0','1','2020-11-01','1','8:00発9:00着ANA128便を利用する');
+
+/* 就職活動申請・報告のデータ(遅刻する場合) */
+INSERT INTO application_and_report(examination_report_id,examination_status_id,action_id,action_place,action_day,action_end_day,company_name,action_status_id,attendance_id,attendance_day,lodging_day_id,information)
+VALUES('0000000012','2','1','北海道情報専門学校','2020-11-01 10:30','2020-11-01 12:30','株式会社HCS','0','2','2020-11-01 13:00','0');
