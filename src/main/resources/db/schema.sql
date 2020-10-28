e129866e069059ca996a2e225b5457b46f439314
s20183045 <s20183045@HCS19064.stdlan.hcs.local>
2020-10-28 13:33:41 +0900

    報告内容の追加

--- src/main/resources/db/schema.sql
+++ src/main/resources/db/schema.sql
@@ -55,6 +55,7 @@ CREATE TABLE IF NOT EXISTS application_and_report (
   attendance_id VARCHAR(10), /* 出欠ID */
   attendance_day DATETIME, /* 出欠日時 */
   lodging_day_id VARCHAR(10), /* 宿泊ID */
-  information VARCHAR(300) /* 連絡事項 */
+  information VARCHAR(300), /* 連絡事項 */
+  contents_report VARCHAR(300),/* 報告内容 */
   CONSTRAINT user_id FOREIGN KEY (user_id) REFERENCES m_user(user_id)
 );
