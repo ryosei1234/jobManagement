a082b1004e5dd5714ce3449e1c1849a5f4d91326
s20183010 <s20183010@HCS19072.stdlan.hcs.local>
2020-10-29 12:00:18 +0900

    サービス作成完了

--- src/main/java/jobhuntingreport/JobHuntingService.java
+++ src/main/java/jobhuntingreport/JobHuntingService.java
@@ -1,21 +1,115 @@
 package jobhuntingreport;
 
+import java.io.IOException;
+import java.nio.file.FileSystem;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
 
-import jp.ac.hcs.white.examreport.ExamReportRepository;
-
 public class JobHuntingService {
 	@Autowired
-	ExamReportRepository examRepository;
+	JobHuntingRepository jobRepository;
 
 	/**
-	 * 受験報告を全権取得する
+	 *  就職活動書を全権取得する
 	 * @param user_id ログイン情報
-	 * @return ExamReportEntity
+	 * @return JobHuntingEntity
 	 * @throws DataAccessException
 	 */
 	public JobHuntingEntity selectAll(String user_id) throws DataAccessException {
-		return examRepository.selectAll(user_id);
+		return jobRepository.selectAll(user_id);
+	}
+
+	/**
+	 * 指定した 就職活動IDの受験報告情報を取得する
+	 * @param examination_report_id  就職活動ID
+	 * @return JobHuntingData
+	 */
+	public JobHuntingData selectOne(String examination_report_id) {
+		return jobRepository.selectOne(examination_report_id);
+
+	}
+
+	/**
+	 *  就職活動申請を一件追加する
+	 * @param jobdata 追加する 就職活動情報
+	 * @return 処理結果(成功:true, 失敗:false)
+	 * @throws DataAccessException
+	 */
+	public boolean insertOne(JobHuntingData jobdata) throws DataAccessException {
+		int rowNumber = jobRepository.insertOne(jobdata);
+		boolean result = (rowNumber> 0) ? true : false;
+		return result;
+	}
+
+	/**
+	 *  就職活動の検索結果を取得
+	 * @param search_examination_report_id 検索したい就職活動ID
+	 * @param serch_action_day 検索したい開始日時
+	 * @param search_user_id 検索したいユーザID
+	 * @param search_company_name 検索したい企業名
+	 * @return ExamReportEntity
+	 */
+	public JobHuntingEntity search(String search_examination_report_id,String serch_action_day, String search_user_name, String search_company_name) {
+
+		JobHuntingEntity jobEntity = null;
+
+		jobEntity = jobRepository.jobSearch(search_examination_report_id, serch_action_day, search_user_name, search_company_name);
+
+		return jobEntity;
+	}
+
+	/**
+	 * 情報をCSVファイルとしてサーバに保存する.
+	 * @throws DataAccessException
+	 */
+	public void saveCsv() throws DataAccessException {
+
+		jobRepository.saveCsv();
+	}
+
+	/**
+	 * サーバーに保存されているファイルを取得して、byte配列に変換する.
+	 * @param fileName ファイル名
+	 * @return ファイルのbyte配列
+	 * @throws IOException ファイル取得エラー
+	 */
+	public byte[] loadCsv(String fileName) throws IOException {
+
+		FileSystem fs = FileSystems.getDefault();
+		Path p = fs.getPath(fileName);
+		byte[] bytes = Files.readAllBytes(p);
+
+		return bytes;
+	}
+
+	/**
+	 * 就職活動を一件分変更する
+	 * @param jobdata 就職活動情報
+	 * @param examination_report_id 就職活動ID
+	 * @return 処理結果(成功:true, 失敗:false)
+	 * @throws DataAccessException
+	 */
+	public boolean updateOne(JobHuntingData jobdata, String examination_report_id) throws DataAccessException {
+
+		int rowNumber = jobRepository.updatereport(jobdata, examination_report_id);
+		boolean result = (rowNumber > 0) ? true : false;
+		return result;
+	}
+
+	/**
+	 * 就職活動を承認変更する
+	 * @param examination_report_id　就職活動ID
+	 * @param examination_status_id　状態内容
+	 * @return 処理結果(成功:true, 失敗:false)
+	 * @throws DataAccessException
+	 */
+	public boolean examstatus(String examination_report_id, String examination_status_id) throws DataAccessException {
+		int rowNumber = jobRepository.statusOne(examination_report_id, examination_status_id);
+		boolean result = (rowNumber > 0) ? true : false;
+		return result;
 	}
 }
