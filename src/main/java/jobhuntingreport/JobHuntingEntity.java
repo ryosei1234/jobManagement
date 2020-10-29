da50415963c86cb0ca69904b2829e836b45ce7f4
s20183040 <s20183040@HCS19063.stdlan.hcs.local>
2020-10-29 12:13:31 +0900

    エラー消したお☆

--- src/main/java/jobhuntingreport/JobHuntingEntity.java
+++ src/main/java/jobhuntingreport/JobHuntingEntity.java
@@ -3,6 +3,9 @@ package jobhuntingreport;
 import java.util.ArrayList;
 import java.util.List;
 
+import lombok.Data;
+
+@Data
 public class JobHuntingEntity {
 	/** 就職活動申請情報のリスト */
 	private List<JobHuntingData> joblist = new ArrayList<JobHuntingData>();
