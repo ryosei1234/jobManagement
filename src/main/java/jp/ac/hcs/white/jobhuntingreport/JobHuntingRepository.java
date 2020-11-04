fdc74969864b4fb4bdf6d61a4f6b021d087c5f0f
s20183045 <s20183045@HCS19064.stdlan.hcs.local>
2020-11-04 13:49:38 +0900

    状態変更用の処理statusOneの追加

--- src/main/java/jp/ac/hcs/white/jobhuntingreport/JobHuntingRepository.java
+++ src/main/java/jp/ac/hcs/white/jobhuntingreport/JobHuntingRepository.java
@@ -31,9 +31,11 @@ public class JobHuntingRepository {
 	/** SQL 就職活動申請・報告書検索*/
 	private static final String SQL_SEARCH_BY_EXAMINATION_STATUS_ID_AND_USER_NAME_AND_COMPANY_NAME ="SELECT * FROM application_and_report app, m_user user WHERE app.user_id = user.user_id AND app.examination_status_id LIKE ? AND app.action_day LIKE ? AND user.user_name LIKE ? and app.company_name LIKE ?";
 	/** SQL 就職活動申請更新*/
-	private static final String SQL_UPDATE_APPLICATION = "UPDATE application_and_report SET examination_status_id = ?,action_id = ?,action_place = ?,action_day = ?,action_end_day = ?,company_name = ?,action_status_id = ?,attendance_id = ?,attendance_day = ?,lodging_day_id = ?,information = ?,schedule = ? WHERE  examination_report_id = ?";
-	/** SQL 就職活動報告更新*/
-	private static final String SQL_UPDATE_AND_REPORT = "UPDATE application_and_report SET contents_report = ? WHERE  examination_report_id = ?";
+	private static final String SQL_UPDATE_APPLICATION = "UPDATE application_and_report SET examination_status_id = ?,action_id = ?,action_place = ?,action_day = ?,action_end_day = ?,company_name = ?,action_status_id = ?,attendance_id = ?,attendance_day = ?,lodging_day_id = ?,information = ?,schedule = ?,contents_report = ? WHERE  examination_report_id = ?";
+	///** SQL 就職活動報告更新*/
+	//private static final String SQL_UPDATE_AND_REPORT = "UPDATE application_and_report SET contents_report = ? WHERE  examination_report_id = ?";
+	/* SQL 就職活動状態変更 */
+	private static final String SQL_UPDATE_ID = "UPDATE application_and_report SET examination_status_id = ?,action_id = ?,action_status_id = ?,attendance_id = ?,lodging_day_id = ? WHERE  examination_report_id = ?";
 	/** SQL CSV出力*/
 	private static final String SQL_SELECT_CSV = "SELECT * FROM application_and_report JOIN m_user ON application_and_report.user_id = m_user.user_id order by company_name_top";
 	@Autowired
@@ -170,6 +172,22 @@ public class JobHuntingRepository {
 				);
 			return rowNumber;
 	}
+	/**
+	 * application_and_reportテーブルの状態変更をする
+	 *
+	 */
+	public int statusOne(JobHuntingData JobHuntingData,String examination_report_id, String examination_status_id) throws DataAccessException {
+		int rowNumber = jdbc.update(SQL_UPDATE_ID,
+				JobHuntingData.getExamination_status_id(),
+				JobHuntingData.getAction_id(),
+				JobHuntingData.getAction_status_id(),
+				JobHuntingData.getAttendance_id(),
+				JobHuntingData.getLodging_day_id(),
+				examination_report_id
+				);
+			return rowNumber;
+	}
+
 	/**
 	 * application_and_reportテーブルのデータを1件追加する
 	 * @param data
