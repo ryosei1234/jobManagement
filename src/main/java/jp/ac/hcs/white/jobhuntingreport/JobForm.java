0a10461301170f52f3504677ece1b8b68ed9da0e
s20183040 <s20183040@HCS19063.stdlan.hcs.local>
2020-11-04 13:30:37 +0900

    あんぱんまんは君さ～

--- src/main/java/jp/ac/hcs/white/jobhuntingreport/JobForm.java
+++ src/main/java/jp/ac/hcs/white/jobhuntingreport/JobForm.java
@@ -1,15 +1,15 @@
 package jp.ac.hcs.white.jobhuntingreport;
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
 import lombok.Data;
 
 @Data
 public class JobForm {
 
-@NotBlank(message = "{require_check}")
+@NotNull(message = "{require_check}")
 private String action_day;
-
 
 private String action_end_day;
 
@@ -28,7 +28,7 @@ private String attendance_id;
 @NotBlank(message = "{require_check}")
 private String attendance_day;
 
-@NotBlank(message = "{require_check}")
+
 private String attendance_end_day;
 
 @NotBlank(message = "{require_check}")
