b097ec3e6bdb214e1f1ec75cb77e0ad47809fb00
s20183032 <s20183032@HCS19071.stdlan.hcs.local>
2020-11-06 14:41:12 +0900

    更新

--- src/main/java/jp/ac/hcs/white/jobhuntingreport/JobFormForStatus.java
+++ src/main/java/jp/ac/hcs/white/jobhuntingreport/JobFormForStatus.java
@@ -1,12 +1,16 @@
 package jp.ac.hcs.white.jobhuntingreport;
 
+import javax.validation.constraints.NotNull;
+
 import lombok.Data;
 
 @Data
 public class JobFormForStatus {
 
+	@NotNull(message = "{require_check}")
 	private String examination_report_id;
 
+	@NotNull(message = "{require_check}")
 	private String examination_status_id;
 
 }
