1cde33ac70edca31eb70ab42b03033162dac7a8e
s20183010 <s20183010@HCS19072.stdlan.hcs.local>
2020-11-06 11:54:33 +0900

    あくせさりー消したお^^わり^^

--- src/main/java/jp/ac/hcs/white/jobhuntingreport/JobHuntingData.java
+++ src/main/java/jp/ac/hcs/white/jobhuntingreport/JobHuntingData.java
@@ -11,115 +11,95 @@ public class JobHuntingData {
 	 * 就職活動申請・報告ID
 	 * 必須入力
 	 */
-	private String examination_report_id;{
-	}
+	private String examination_report_id;
 
 	/*
 	 * ユーザID
 	 */
-	private String user_id;{
-	}
+	private String user_id;
 
 	/*
 	 * 就職活動申請・報告状態ID
 	 */
-	private String examination_status_id;{
-	}
+	private String examination_status_id;
 
 	/*
 	 * 活動内容ID
 	 */
-	private String action_id;{
-	}
+	private String action_id;
 
 	/*
 	 * 活動場所
 	 */
-	private String action_place;{
-	}
+	private String action_place;
 
 	/*
 	 * 活動開始日時
 	 */
-	private String action_day;{
-	}
+	private String action_day;
 
 	/*
 	 * 活動終了日時
 	 */
-	private String action_end_day;{
-	}
+	private String action_end_day;
 
 	/*
 	 * 企業名
 	 */
-	private String company_name ;{
-	}
+	private String company_name ;
 
 	/*
 	 * 就職活動続行ID
 	 */
-	private String action_status_id;{
-	}
+	private String action_status_id;
 
 	/*
 	 * 出欠ID
 	 */
-	private String attendance_id;{
-	}
+	private String attendance_id;
 
 	/*
 	 * 出欠日時
 	 */
-	private String attendance_day;{
-	}
+	private String attendance_day;
 
 	/*
 	 * 出欠終了日時
 	 */
-	private String attendance_end_day;{
-	}
+	private String attendance_end_day;
 
 	/*
 	 * 宿泊ID
 	 */
-	private String lodging_day_id;{
-	}
+	private String lodging_day_id;
 
 	/*
 	 * 連絡事項
 	 */
-	private String information;{
-	}
+	private String information;
 
 	/*
 	 * スケジュール
 	 */
-	private String schedule;{
-	}
+	private String schedule;
 
 	/*
 	 * 報告内容
 	 */
-	private String contents_report;{
-	}
+	private String contents_report;
 
 	/*
 	 * ユーザ名
 	 */
-	private String user_name;{
-	}
-
+	private String user_name;
 
 	/*
 	 * ユーザのクラス
 	 */
-	private String user_class;{
-	}
+	private String user_class;
 
 	/*
 	 * ユーザの出席番号
 	 */
-	private String user_student_no;{
-	}
+	private String user_student_no;
 }
